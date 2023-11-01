21feea0e57bc39721821f94536206df708653b08
jonathafernandes <jonathafernandes@live.com>
2023-11-01 12:20:12 -0300

    feat(missionary work): change content

--- src/pages/MissionaryWork.jsx
+++ src/pages/MissionaryWork.jsx
@@ -1,5 +1,14 @@
+import { Footer } from "../components/Footer";
+import { Header } from "../components/Header";
+
+import styles from "./MissionaryWork.module.css";
+
 export function MissionaryWork() {
     return (
-        <h2>Em breve...</h2>
+        <div className={styles.content}>
+            <Header />
+            <h2>Em breve...</h2>
+            <Footer />
+        </div>
     )
 }
