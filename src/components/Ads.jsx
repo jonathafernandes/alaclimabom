b328ba883b4926adcc6834ee4c94ff437b318356
jonathafernandes <jonathafernandes@live.com>
2023-12-16 12:21:55 -0300

    feat: monthly updates

--- src/components/Ads.jsx
+++ src/components/Ads.jsx
@@ -6,44 +6,12 @@ export function Ads() {
 
     const ads = [
         {
-            id: 0,
-            tag: 'Chamado',
-            title: '',
-            date: 'Mudança na Presidência do Quórum de Élderes',
-            content: 'No último dia 12 de novembro, durante a Reunião Sacramental, o irmão Inácio Ferreira da Silva foi apoiado e ordenado como Segundo Conselheiro do Quórum de Élderes.'
-        },
-        {
             id: 1,
-            tag: 'Coral',
-            title: 'Ensaio do coral',
-            date: 'Todos os domingos — 17h00',
-            content: 'O ensaio do coral é realizado todos os domingos junto com a Ala Colina. Todos estão convidados!'
-        },
-        {
-            id: 2,
-            tag: 'Natal',
-            title: 'Contraternização de Natal',
-            date: '08 de Dezembro — 18h00',
-            content: ''
-        },
-        {
-            id: 3,
-            tag: 'Natal',
-            title: 'Concerto de Natal',
-            date: '09 de Dezembro',
-            content: {
-                __html: 'Caso queira participar do nosso concerto de Natal, clique <a style="color: blue;" href="https://chat.whatsapp.com/JwRR3yd58zZH4t2THU0LUC" target="_blank" rel="noreferrer noopener">aqui</a> para entrar no grupo do WhatsApp.'
-            }
-        },
-        {
-            id: 4,
-            tag: 'Templo',
-            title: 'Caravana ao templo',
-            date: '15 de Dezembro',
-            content: {
-                __html: 'Será realizada em Dezembro, e é pedido desde já que aqueles que têm interesse reservem suas vagas e efetuem o pagamento com antecedência. Para reservar sua vaga, basta clicar <a style="color: blue;" href="https://docs.google.com/forms/d/e/1FAIpQLScmdWiAD86Eml1yu23tjN6F_i-_uoYC1uWYz5JO_ccDxUK8eA/viewform" target="_blank" rel="noreferrer noopener">aqui</a> e preencher o formuláro. Para mais informações, procurar a Presidência do Quórum de Elderes.'
-            }
-        },
+            tag: 'Jovens',
+            title: 'Formatura do Seminário e Instituto',
+            date: '17 de Dezembro — 19h00',
+            content: 'O evento será realizado na seda da Estaca Maceió Brasil.'
+        }
     ]
 
     return (
@@ -52,6 +20,7 @@ export function Ads() {
             <ul className={adsStyles.adsList}>
                 {ads.map((item) => (
                     <React.Fragment key={item.id}>
+                        {ads == "" ? 'Nenhum anúncio no momento...' :
                         <li>
                             <span className={postStyles.tag}>
                                 {item.tag}
@@ -65,7 +34,7 @@ export function Ads() {
                                 ) : (
                                 <p>{item.content}</p>
                             )}
-                        </li>
+                        </li>}
                     </React.Fragment>
                 ))}
             </ul>
--- src/components/Birthdays.jsx
+++ src/components/Birthdays.jsx
@@ -3,23 +3,19 @@ import styles from "./Birthdays.module.css";
 
 export function Birthdays() {
     const birthdaysList = [
-        { id: 1, date: '2 nov', name: 'Eclesyaste Gomes' },
-        { id: 2, date: '3 nov', name: 'Ana Carolina' },
-        { id: 3, date: '5 nov', name: 'Kevin Henrique' },
-        { id: 4, date: '7 nov', name: 'Jonatha Fernandes' },
-        { id: 5, date: '9 nov', name: 'Maria Cícera' },
-        { id: 6, date: '11 nov', name: 'Fabio da Silva' },
-        { id: 7, date: '13 nov', name: 'Cícero Bertolino' },
-        { id: 8, date: '13 nov', name: 'Alice Nayara' },
-        { id: 9, date: '19 nov', name: 'Helena Santos' },
-        { id: 10, date: '23 nov', name: 'Marili Guedes' },
-        { id: 11, date: '23 nov', name: 'Soleide Silva' },
-        { id: 12, date: '25 nov', name: 'João Pedro' },
-        { id: 13, date: '26 nov', name: 'Marcio Antônio' },
-        { id: 14, date: '26 nov', name: 'Kajitaisma Oliveira' },
-        { id: 15, date: '27 nov', name: 'Anne Caroline' },
-        { id: 16, date: '27 nov', name: 'Inácio Ferreira' },
-        { id: 17, date: '27 nov', name: 'Nadja Torres' },
+        { id: 1, date: '2 dez', name: 'Maria Gabriela' },
+        { id: 2, date: '5 dez', name: 'Maria Jose dos Santos' },
+        { id: 3, date: '6 dez', name: 'Jhon Erick' },
+        { id: 4, date: '7 dez', name: 'Stephany Brenda' },
+        { id: 5, date: '7 dez', name: 'Vitor Gabriel' },
+        { id: 6, date: '9 dez', name: 'Maria José da Silva' },
+        { id: 7, date: '10 dez', name: 'Nayane Paula' },
+        { id: 8, date: '14 dez', name: 'Ubirajara Felix' },
+        { id: 9, date: '17 dez', name: 'Marcos Emanoel' },
+        { id: 10, date: '17 dez', name: 'Bruno Willian' },
+        { id: 11, date: '25 dez', name: "Daher Tutt's" },
+        { id: 12, date: '30 dez', name: 'Brenda Sergio' },
+        { id: 13, date: '31 dez', name: 'Mayara Monteiro' }
     ];
 
     return (
