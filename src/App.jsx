7f70cb9e5ee6868636e6569de79352823bc0a091
jonathafernandes <jonathafernandes@live.com>
2024-06-03 13:49:19 -0300

    feat: remove content posts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { Hero } from "./components/Hero";
-import { Post } from "./components/Post";
 
 import "./index.css";
 
@@ -12,16 +11,12 @@ export function App() {
       <Header />
       <main>
         <Hero />
-        <h3>NOTÍCIAS RECENTES</h3>
-        <Post
-          author={'Quórum de Elderes — Ewerton Chrysostomo'}
-          textContent={'Queridos irmãos, tenho refletido sobre o verdadeiro significado de amar a Deus. O Senhor afirmou: "Se me amais, guardais os meus mandamentos" (João 14:15). Quando amamos alguém, desejamos estar próximos e estamos dispostos a sacrificar-nos para estar junto da pessoa amada. No entanto, quando não amamos sinceramente, não nos importamos em estar próximos. Um dos lugares em que podemos nos aproximar mais do Senhor são os edifícios da igreja e os templos sagrados. A pergunta que devemos fazer a nós mesmos toda semana é: "Eu amo verdadeiramente o Senhor?" Se a resposta for afirmativa, devemos esforçar-nos para guardar os mandamentos, planejando-nos para frequentar Sua casa. Estabeleçam metas, empenhem-se com dedicação constante e fortaleçam o amor ao Senhor.'}
-          lessonDate={''}
-          lessonTitle={''}
-          teacher={'Em breve...'}
-          />
+        <h2>Em breve</h2>
+        <br />
+        <br />
+        <br />
       </main>
-      <Footer />
+      <Footer />  
     </>
   )
 }
--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,62 +1,50 @@
 import styles from "./Post.module.css";
 
-import imgReliefSocietyMissionaries from "../assets/relief-society-missionaries.png";
-import imgNewBishopric from "../assets/new-bishopric.png";
 import PropTypes from 'prop-types';
 
 import { Ads } from "./Ads";
-import { New } from "./New";
 
 export function Post(props) {
     return (
-        <div className={styles.content} id="post">
-            <header>
-                <span className={styles.date}>08/12 à 07/01 <div className={styles.line}></div></span>
-            </header>
-            <div className={styles.contentPost}>
-                <section className={styles.news}>
-                    < New 
-                        title={'Cestas para os missionários'}
-                        description={'As irmãs da Sociedade de Socorro uniram esforços para arrecadar alimentos, com o propósito de preparar cestas para presentear os missionários.'}
-                        src={imgReliefSocietyMissionaries}
-                    />
-                    < New 
-                        title={'Novo bispado'}
-                        description={'No dia 07/01, o Presidente da Estaca, Manoel Messias, anunciou o novo bispado da ala. Clesivan Carlos foi chamado como novo Bispo da ala junto com os seus conselheiros, Bruno William e Antônio Veríssimo.'}
-                        src={imgNewBishopric}
-                    />
-                </section>
-                
-                <div className={styles.bottom}>
-                    <section>
-                        <h3>Mensagem para os membros</h3>
-                        <span className={styles.tag}>{props.author}</span>
-                        <br />
-                        <p className={styles.contentMessage}>
-                            {props.textContent}
-                        </p>
-                        <div className={styles.lesson}>
-                            <h3>Lição do Quórum de Élderes</h3>
-                            <span>Próxima aula: {props.lessonDate}</span>
-                            <a href="https://www.churchofjesuschrist.org/study/general-conference/2023/10/16ardern?lang=por" target="_blank" rel="noreferrer">
-                                <div className={styles.contentLesson}>
-                                    {/*TO REMEMBER: manter estrutura
-
-                                    <img className={styles.lessonImg} src={} alt="" /> */}
-                                    <div>
-                                        <p>{props.lessonTitle}</p>
-                                        <span>{props.teacher}</span>
+        <>
+            <h3>NOTÍCIAS RECENTES</h3>
+            <div className={styles.content} id="post">
+                <header>
+                    <span className={styles.date}>08/12 à 07/01 <div className={styles.line}></div></span>
+                </header>
+                <div className={styles.contentPost}>
+                    <section className={styles.news}>
+                    </section>
+                    
+                    <div className={styles.bottom}>
+                        <section>
+                            <h3>Mensagem para os membros</h3>
+                            <span className={styles.tag}>{props.author}</span>
+                            <br />
+                            <p className={styles.contentMessage}>
+                                {props.textContent}
+                            </p>
+                            <div className={styles.lesson}>
+                                <h3>Lição do Quórum de Élderes</h3>
+                                <span>Próxima aula: {props.lessonDate}</span>
+                                <a href="#" target="_blank" rel="noreferrer">
+                                    <div className={styles.contentLesson}>
+                                        <img className={styles.lessonImg} src={""} alt="" />
+                                        <div>
+                                            <p>{props.lessonTitle}</p>
+                                            <span>{props.teacher}</span>
+                                        </div>
                                     </div>
-                                </div>
-                            </a>
-                        </div>
-                    </section>
-                    <section>
-                        <Ads />
-                    </section>
+                                </a>
+                            </div>
+                        </section>
+                        <section>
+                            <Ads />
+                        </section>
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
